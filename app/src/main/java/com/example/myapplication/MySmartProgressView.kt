2df36fb461ac2e8cafe917e5813d627ecdb26b36
chrisxu <chrisxu@kugou.net>
2021-02-04 19:47:03 +0800

    完善代码

--- app/src/main/java/com/example/myapplication/MySmartProgressView.kt
+++ app/src/main/java/com/example/myapplication/MySmartProgressView.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication
 
 import android.content.Context
-import android.graphics.Color
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
@@ -64,6 +64,11 @@ class MySmartProgressView (context: Context, attributeSet: AttributeSet?, style:
     private val bgCircleColorClean = getContext().getColor(R.color.progress_bg_circle_color_clear)
     private val indicatorColorClean = getContext().getColor(R.color.progress_indicator_color_clear)
     private var indicatorColor : Int = 0
+    /**
+     * 保温的进度圆变色值
+     */
+    private val mLinearGradientColor1 = getContext().getColor(R.color.progress_linearGradient_color1)
+    private val mLinearGradientColor2 = getContext().getColor(R.color.progress_linearGradient_color2)
 
     constructor(context: Context,parentWidth :Int,outerShaderWidth:Int,circleStrokeWidth:Int,cleanMode:Boolean,
                 isKeepWare:Boolean):this(context){
@@ -77,21 +82,87 @@ class MySmartProgressView (context: Context, attributeSet: AttributeSet?, style:
 
     fun init(){
         initView()
-
-//        initPaint()
-//
-//        initBitmap()
-//
+        initPaint()
+        initBitmap()
 //        initAnim()
     }
+
+    private var centerX  = 0
+    private var centerY = 0
+    private var radius = 0
 
     private fun initView(){
         indicatorColor = indicatorColor1
         val transparentColor = Color.parseColor("#00000000")
+        radialGradientColors[0] = transparentColor
+        radialGradientColors[1] = transparentColor
+        radialGradientColors[2] = transparentColor
+        radialGradientColors[3] = transparentColor
+
+        centerX = parentWidth / 2
+        centerY = parentWidth / 2
+
+        //粒子圆环的宽度
+        radius = centerX - outerCircleStrokeWidth/2 - outerShaderWidth
+    }
+
+    private val sweepPaint = Paint()
+    private val mRadialGradientStops = floatArrayOf(0f, 0.69f, 0.86f, 0.94f, 0.98f, 1f)
+    private lateinit var radialGradient : RadialGradient
+    private lateinit var sweepGradient : SweepGradient
+    private val arcPath : Path = Path()
+    private val outerCirclePaint = Paint()
+    private val pointPaint = Paint()
+    private val backCirclePaint = Paint()
+    private val backShadePaint = Paint()
+    private val bmpPaint = Paint()
+    private lateinit var bitmapDst : Bitmap
+
+    private fun initPaint(){
+        sweepPaint.isAntiAlias = true
+        sweepPaint.style = Paint.Style.FILL_AND_STROKE
+        radialGradient = RadialGradient(0f,0f,centerX.toFloat(),radialGradientColors,mRadialGradientStops,
+            Shader.TileMode.CLAMP)
+
+        sweepPaint.setShader(radialGradient)
+
+        //step1：初始化外层圆环的画笔
+        outerCirclePaint.style = Paint.Style.STROKE
+        outerCirclePaint.isAntiAlias = true
+        outerCirclePaint.strokeWidth = outerCircleStrokeWidth.toFloat()
+
+        val gradientColors = intArrayOf(mLinearGradientColor2,mLinearGradientColor2,
+            mLinearGradientColor1,mLinearGradientColor1,
+            mLinearGradientColor2,mLinearGradientColor2
+            )
+        val positions = floatArrayOf(0f,0.05f,0.12f,0.88f,0.95f,1f)
+        sweepGradient = SweepGradient(0f,0f,gradientColors,positions)
+
+        //stop2：运动粒子的画笔
+
+        //step3：初始化底色圆画笔
+        backCirclePaint.isAntiAlias = true
+        backCirclePaint.strokeWidth = outerCircleStrokeWidth.toFloat()
+        backCirclePaint.style = Paint.Style.STROKE
+        //初始化底色盘的initAnimator画笔
+        backShadePaint.isAntiAlias = true
+        backShadePaint.color = bgCircleColor1
+
+        //指针画笔
+    }
+
+    private fun initBitmap(){
+        val f = 130f / 656f
+        bitmapDst = BitmapFactory.decodeResource(resources,R.drawable.indicator)
+        val bitmapDstHeight = width * f
+        val bitmapDstWidth = bitmapDstHeight * bitmapDst.width / bitmapDst.height
 
     }
 
-    init {
-
+    private fun getSectorClip(r: Float,angle:Float,sweep:Float){
+        arcPath.reset()
+        arcPath.addArc(-r,-r,r,r,angle,sweep)
+        arcPath.lineTo(0f,0f)
+        arcPath.close()
     }
 }
